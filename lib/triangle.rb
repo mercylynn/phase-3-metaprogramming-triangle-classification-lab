f8ad77f738521df1ed1b42c20fa3b709cae40d4d
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-26 11:25:11 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_reader :length ,:width ,:height
+  def initialize(length,width,height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind 
+    if @length <= 0 || @width <= 0 || @height <= 0
+      raise TriangleError
+    elsif @length + @width <= @height || @length + @height <= @width || @width + @height <= @length
+      raise TriangleError
+    else
+      return :equilateral if @length == @width && @width == @height
+      return :isosceles if @length == @width || @length == @height || @width == @height
+      return :scalene if @length != @width && @length != @height && @width != @height
+    end
+  end
+
+
+  class TriangleError < StandardError
+    
+  end
 end
+
